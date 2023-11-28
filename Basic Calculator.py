00b92a6a2e26b28bb53db05f474694c9a563b41a
J Iqbal <msg2jiqbal@gmail.com>
2023-11-27 20:29:29 -0800

    Modified Calculator

--- Basic Calculator.py
+++ Basic Calculator.py
@@ -4,6 +4,7 @@
 # Delhi Technological University (formerly, Delhi College of Engineering)
 # New Delhi, India
 
+import math
 
 #Python program to create a simple calculator 
   
@@ -21,23 +22,32 @@ def multiply(number1, number2):
   
 #Function to perform division of two numbers 
 def divide(number1, number2): 
-    return (number1 / number2)
+    result = (number1 / number2)
+# Modified divide function to round the result for 2 decimal digits
+    result = round(result, 2)
+    return (result)
+# Added function to computer power/exponent of a number
+def power(number, exp):
+    print(math.pow(number, exp))
+
   
-print("Operation Menu -\n" \ 
-        "1. Addition of two numbers\n" \ 
-        "2. Subtraction of two numbers\n" \ 
-        "3. Multiplication of two numbers\n" \ 
-        "4. Division of two numbers\n") 
+print("Operation Menu -\n" 
+        "1. Addition of two numbers\n" 
+        "2. Subtraction of two numbers\n"  
+        "3. Multiplication of two numbers\n"  
+        "4. Division of two numbers\n"
+        "5. Exponent of a number\n") 
   
   
 #Taking input from the user  
-select = input("Select operations form 1, 2, 3, 4 :") 
+select = input("Select operations form 1, 2, 3, 4, 5:") 
   
 num1 = int(input("Enter first number: ")) 
 num2 = int(input("Enter second number: ")) 
   
 if select == '1': 
-    print(num1, "+", numb2, "=", 
+# Corrected => replaced numb2 with num2
+    print(num1, "+", num2, "=", 
                     add(num1, num2)) 
   
 elif select == '2': 
@@ -51,5 +61,9 @@ elif select == '3':
 elif select == '4': 
     print(num1, "/", num2, "=", 
                     divide(num1, num2)) 
+elif select == '5': 
+    print(num1, "^", num2, "=", 
+                    pow(num1, num2)) 
 else: 
-    print("Invalid input!"
+# Corrected => closing parenthesis was missing     
+    print("Invalid input!")
